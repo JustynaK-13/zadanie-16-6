d9372bf5b3f84e0432122d155436ab037be57bf5
Justyna Kaczorowska <js.kaczorowska@gmail.com>
2018-05-22 12:32:38 +0200

    Change content

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,14 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 //webpack.config.js
-module.exports = (env) => {
-    return {
-        mode: env || 'production',
-        entry: './src/index.js',
+module.exports = {
+    entry: './src/index.js',
         output: {
             path: path.resolve(__dirname, 'build'),
             filename: 'app.bundle.js'
         },
         
-        module: {
+    module: {
             rules: [
                 {
                     test: /\.js$/,
@@ -32,10 +30,10 @@ module.exports = (env) => {
                 }
             ]
         }
-    }
     plugins: [new HtmlWebpackPlugin({
       template: 'src/index.html',
       filename: 'index.html',
       inject: 'body'
-    })]
+      })
+    ]
 };
